45e86d48b7d1499e0973c27bf9bb01a7ed29e44f
roy-amelie <roydorelon.amelie@gmail.com>
2020-07-10 12:50:28 +0200

    find just one letter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::iter::Iterator;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    	let secret_letter = 'b';
+
+	loop{
+
+		println!("please choice a letter");
+
+		let mut input = String::new();
+
+		io::stdin()
+		.read_line(&mut input)
+		.expect("failed to read line");
+		
+		let guess_vec: Vec<char> = input.chars().collect();
+		let guess = guess_vec[0];
+	 	if secret_letter == guess {
+			println!("bravo vous avez trouver la lettre {}",secret_letter);
+			break;
+		} else {
+			println!("ce n'est pas la bonne lettre")
+		}
+	}
 }
